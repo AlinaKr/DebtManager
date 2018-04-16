94912519e6bb62dc9a44f76763a9d8dde206cac2
AlinaKr <33060131+AlinaKr@users.noreply.github.com>
2018-04-16 21:30:16 +0200

    Revert "blahblah"

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		DebtCalculator.main("C:\\Users\\AlinaKraft\\Documents\\IT\\WhoOwesWhom\\debts.txt");
-	}
-
-}
